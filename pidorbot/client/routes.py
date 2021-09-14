4a4e92ac7f7c85c973341b914560d3be20b9941a
qerdcv <vadym.tishchenko2@gmail.com>
2021-09-14 20:46:25 +0300

    Rename api to handlers

--- pidorbot/client/routes.py
+++ pidorbot/client/routes.py
@@ -1,10 +1,10 @@
 from aiohttp import web
 from client.config import BASE_PATH
-from client import api
+from client import handlers
 
 routes = [
-    web.get('/ping', api.ping),
-    web.get('/{chat_id}', api.index),
-    web.get('/media/{file_name}', api.media),
+    web.get('/ping', handlers.ping),
+    web.get('/{chat_id}', handlers.index),
+    web.get('/media/{file_name}', handlers.media),
     web.static('/static', BASE_PATH / 'static')
 ]
