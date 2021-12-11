8bd1b43b6f364a91732ea81b2e521fef5fa373b2
Amogus <600dan600@gmail.com>
2021-12-11 15:12:59 +0200

    remake helpers

--- pidorbot/src/helpers.py
+++ pidorbot/src/helpers.py
@@ -5,21 +5,18 @@ from src import db
 def update_streak(chat_id: int, chat_stat: db.ChatStat, winner: db.User):
     if chat_stat is None:
         best_streak = 1
-        db.update_user_streak(chat_id, winner.user_id, streak=False)
-        db.update_user_best_streak(chat_id, winner.user_id, best_streak)
+        db.update_streak(chat_id=chat_id, user_id=winner.user_id, current_streak=best_streak)
     else:
-        user_id = db.get_user_id(chat_id, chat_stat.last_choice)
         best_streak = 0
+        user_id = db.get_user_id(chat_id, chat_stat.last_choice)
+        streak = db.get_streak(chat_id, winner.user_id)
         if user_id == winner.user_id:
-            db.update_user_streak(chat_id, winner.user_id, streak=True)
-            streak = db.get_user_streak(chat_id, user_id)
-            if streak.best_streak < streak.current_streak:
-                best_streak = streak.current_streak
-                db.update_user_best_streak(chat_id, winner.user_id, best_streak)
+            db.update_streak(chat_id, winner.user_id, current_streak=streak.current_streak + 1)
+            streak = streak.current_streak + 1
+            if db.is_best_streak(chat_id=chat_id, user_id=winner.user_id, current_streak=streak):
+                best_streak = streak
         else:
-            db.update_user_streak(chat_id, winner.user_id, streak=False)
-            streak = db.get_user_streak(chat_id, winner.user_id)
+            db.update_streak(chat_id, winner.user_id, 1)
             if streak.best_streak == 0:
                 best_streak = 1
-                db.update_user_best_streak(chat_id, winner.user_id, best_streak)
     return best_streak
