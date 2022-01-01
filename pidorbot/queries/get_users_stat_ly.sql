a73fc454c630f8e2161b03480128d3dd3e1039a3
Vadym Tishchenko <v.tishchenko@evopay.com.ua>
2022-01-01 18:34:50 +0200

    Fix ly query

--- pidorbot/queries/get_users_stat_ly.sql
+++ pidorbot/queries/get_users_stat_ly.sql
@@ -1,5 +1,4 @@
 select user_id, username, count(user_id) as count from p_stat
 where chat_id=?
-  and cast(strftime('%Y', choice_date) as integer) < DATE()
-  and cast(strftime('%Y', choice_date) as integer) > DATE() - 2
+  and cast(strftime('%Y', choice_date) as integer) == DATE() - 1
 group by user_id ORDER BY count DESC LIMIT 10;
