21d78db7a1b3f07f3a5d8d1c9df911a517a95215
Eiphoria <spam.yakovich@gmail.com>
2022-07-14 16:39:07 +0300

    Add filter to API function

--- pidorbot/client/api.py
+++ pidorbot/client/api.py
@@ -14,9 +14,18 @@ class FilterEnum(Enum):
     all_time = 'all_time'
 
 
-async def get_users(request: web.Request):
+async def get_users(request: web.Request) -> web.Response:
+    active_filter = request.query.get('filter')
+    if active_filter not in FilterEnum.__members__:
+        active_filter = 'year'
+    active_filter = FilterEnum[active_filter]
     chat_id = request.match_info.get('chat_id')
-    res = db.get_users_stat_at(chat_id)
-    if res is None:
+    if active_filter == FilterEnum.year:
+        chat_stat = db.get_users_stat(chat_id)
+    elif active_filter == FilterEnum.last_year:
+        chat_stat = db.get_users_stat_ly(chat_id)
+    else:
+        chat_stat = db.get_users_stat_at(chat_id)
+    if chat_stat is None:
         return web.json_response({'message': 'chat not found'}, status=404)
-    return web.json_response(res.to_dict())
+    return web.json_response(chat_stat.to_dict())
